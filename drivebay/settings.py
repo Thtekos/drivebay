c4f0a0553c781d78be7c3825ab0f0a7beeb67fde
Panagiotis Stekos <panagiotisstek@gmail.com>
2026-04-03 11:18:21 +0300

    Install Pillow, fix DEFAULT_AUTO_FIELD warning

--- drivebay/settings.py
+++ drivebay/settings.py
@@ -149,3 +149,5 @@ CSRF_COOKIE_HTTPONLY = True
 # Static files for production
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
