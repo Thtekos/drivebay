cb268d574043eefa879a4fa624d0ac9db9e414e6
Panagiotis Stekos <panagiotisstek@gmail.com>
2026-04-20 22:50:15 +0300

    Improve Django admin panel display for professor review

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
+from .models import Make, CarModel, Car, UserProfile, Review, CartItem, Wishlist, ViewHistory, Purchase
 
-# Register your models here.
+
+@admin.register(Make)
+class MakeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'country']
+    search_fields = ['name']
+
+
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'make']
+    search_fields = ['name', 'make__name']
+    list_filter = ['make']
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'price', 'fuel_type', 'transmission', 'condition', 'is_available', 'created_at']
+    search_fields = ['make__name', 'car_model__name']
+    list_filter = ['fuel_type', 'transmission', 'condition', 'is_available', 'make']
+    list_editable = ['is_available']
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone', 'location']
+    search_fields = ['user__username', 'user__email']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['user', 'car', 'rating', 'created_at']
+    list_filter = ['rating']
+    search_fields = ['user__username', 'car__make__name']
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['user', 'car', 'added_at']
+    search_fields = ['user__username']
+
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ['user', 'car', 'added_at']
+    search_fields = ['user__username']
+
+
+@admin.register(ViewHistory)
+class ViewHistoryAdmin(admin.ModelAdmin):
+    list_display = ['user', 'car', 'viewed_at']
+    search_fields = ['user__username']
+
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ['user', 'car', 'price_paid', 'purchased_at']
+    search_fields = ['user__username']
